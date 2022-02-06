b77ec06e457bc726f5674ab30c000036c36e2b8d
njiti <kamaukan77@gmail.com>
2022-02-06 13:57:44 -0930

    Listener
    added a host listener to react on clicking  button

---
+++ src/app/mostliked.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MostlikedDirective } from './mostliked.directive';
+
+describe('MostlikedDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MostlikedDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/mostliked.directive.ts
@@ -0,0 +1,16 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[appMostliked]'
+})
+export class MostlikedDirective {
+  constructor(private elem:ElementRef) { }
+
+  @HostListener('onsubmit') onSubmit(){
+    this.textDeco("red")
+  }
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.backgroundColor = action
+  }
+}
