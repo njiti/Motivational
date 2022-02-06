4c1608cfc7ac6928912bfd6c99774d3fbe5e2ddc
njiti <kamaukan77@gmail.com>
2022-02-06 13:07:27 -0930

    Update quote-detail.component.ts
    added quote import

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,12 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, Output, EventEmitter} from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote-detail',
   templateUrl: './quote-detail.component.html',
-  styleUrls: ['./quote-detail.component.css']
+  styleUrls: ['./quote-detail.component.scss']
 })
 export class QuoteDetailComponent implements OnInit {
 
+
+  @Input() quote!: Quote
+  @Output() toDelete = new EventEmitter<boolean>()
+
+  deleteQuote(getRid:boolean){
+    this.toDelete.emit(getRid)
+  }
   constructor() { }
 
   ngOnInit(): void {
