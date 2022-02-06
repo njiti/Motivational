8da2b4e84dc21460aed792335a929dd2e8e99af2
njiti <kamaukan77@gmail.com>
2022-02-06 13:14:23 -0930

    Update quote-form.component.ts
    Added imports for Quote, and my components

--- src/app/quote-form/quote-form.component.ts
+++ src/app/quote-form/quote-form.component.ts
@@ -1,11 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote } from '../quote'
 
 @Component({
   selector: 'app-quote-form',
   templateUrl: './quote-form.component.html',
-  styleUrls: ['./quote-form.component.css']
+  styleUrls: ['./quote-form.component.scss']
 })
 export class QuoteFormComponent implements OnInit {
+
+  newQuote = new Quote(0, "", "", "", new Date())
+  @Output() addQuote = new EventEmitter<Quote>()
+
+  submitQuote(){
+    this.addQuote.emit(this.newQuote)
+  }
 
   constructor() { }
 
